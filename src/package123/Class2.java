fa397fa9150647ab172a8eb970b0e39f24352a93
user <user@Dell>
2022-05-08 11:09:50 +0530

    this is sample project

---
+++ src/package123/Class2.java
@@ -0,0 +1,18 @@
+package package123;
+
+public class Class2 {
+	
+	public static void main(String[]args)
+	{
+		int x = 10;
+		if (x<20) {
+			System.out.println(x);
+			System.out.println("This is if statement");
+		}
+		else {
+			System.out.println(x);
+			System.out.println("This is else statement");
+		}
+	}
+
+}
